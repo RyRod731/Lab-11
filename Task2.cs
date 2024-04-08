e061f2cd2a0a341a6654d0aa7e8b2839756a8f31
Ryan Rodriguez <144503255+RyRod731@users.noreply.github.com>
2024-04-08 16:41:43 -0400

    Add files via upload

--- Task2.cs
+++ Task2.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Task2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<string> Names = new List<string>{ "Roger", "Bob", "Adam", "Alec", "Patrick", "Mario", "Luigi", "Ryan", "Michael", "Gabriel", "Raphael", "Uriel", "Daniel", "Karen", "Lucas", "Andres", "Ben", "Hilda", "Zack", "Vincent"};
+    string[] randomNames = new string[15];
+    HashSet<string> nameList = new HashSet<string>();
+    HashSet<string> duplicateNames = new HashSet<string>();
+
+    void Awake()
     {
-        
+        CreateNameArray();
+        CheckDuplicateNames();
+        PrintOutResults();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateNameArray()
     {
-        
+        for(int i = 0; i < 15; i++) 
+        {
+            randomNames[i] = Names[Random.Range(0, Names.Count)];
+        }
+    }
+
+    void CheckDuplicateNames() {
+        for(int i = 0; i < randomNames.Length; i++) {
+            if (!nameList.Add(randomNames[i])) {
+                //If you cannot add the name to the hashset, that means the name is already in the set, so it is a duplicate
+                duplicateNames.Add(randomNames[i]);
+            }
+        }
+    }    
+
+    void PrintOutResults() {
+        string arrayOutput = "Created the name array: ";
+        for(int i = 0; i < randomNames.Length; i++) {
+            CheckForPeriod(i, randomNames.Length, ref arrayOutput, randomNames[i]);
+        }
+
+        string hashOutput = "The array has duplicate names: ";
+        string[] duplicateNamesArray = duplicateNames.ToArray();
+
+        for (int i = 0; i < duplicateNamesArray.Length; i++) {
+            CheckForPeriod(i, duplicateNamesArray.Length, ref hashOutput, duplicateNamesArray[i]);
+        }
+
+        Debug.Log(arrayOutput);
+        Debug.Log(hashOutput);
+    }
+
+    void CheckForPeriod(int loopInt, int loopMaxInt, ref string output, string name) {
+        if (loopInt == loopMaxInt - 1) 
+        {
+            output += name  + ". ";
+        }
+        else 
+        {
+            output += name + ", ";
+        }
     }
 }
